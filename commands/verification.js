2daa9a7ce0f04f14f7c928b18c9dced10555c813
LIIN <53265037+KImCHENLIIN@users.noreply.github.com>
2021-01-25 23:49:34 +0600

    Update verification.js

--- commands/verification.js
+++ commands/verification.js
@@ -31,9 +31,14 @@ module.exports.run = async (bot, message) => {
   ]);
   message.channel
     .send(
-      new global.MessageEmbed().setColor("2CE871")
-        .setTitle(`Добро пожаловать на ${message.guild.name}
-        Для доступа на сервер нажмите - ✅`)
+      new global.MessageEmbed().setColor("#2f3136")
+        .setAuthor('Добро пожаловать на SPACEXH.', 'https://emoji.gg/assets/emoji/bongocat.gif')
+        .setImage('https://media.giphy.com/media/N3yLGQ1oMYfGU/giphy.gif')  
+        .setDescription(`Сейчас ты находишься на верификации, чтобы получить доступ к серверу, нажми на реакцию ниже.
+
+\`\`\`Зачем нужна верификация?\`\`\`
+Она защищает сервер от идиотов, малолетних рейдеров с ипользованием ботов, сторонних программ.<:bancat:765570923398823958>
+`)
     )
     .then(async msg => {
       await global.Collection.Guild.upsertOne(
